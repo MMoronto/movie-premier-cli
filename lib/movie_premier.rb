7018a4fe1a364ad4916561b74f6a1faad4f42968
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-08-28 17:27:09 +0000

    stubbed out console, CLI controller, movie.rb methods and logic statements

---
+++ lib/movie_premier.rb
@@ -0,0 +1,10 @@
+require 'open-uri'
+#require 'pry'
+require 'nokogiri'
+
+require_relative "movie_premier/version"
+require_relative "movie_premier/cli"
+require_relative "movie_premier/movie"
+
+module MoviePremier
+end 
