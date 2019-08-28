8dddad86daaab08a1224aeaf649084b7392eb073
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-08-28 14:29:19 +0000

    first commit

---
+++ lib/movie/premier/cli.rb
@@ -0,0 +1,10 @@
+require "movie/premier/cli/version"
+
+module Movie
+  module Premier
+    module Cli
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/movie/premier/cli/version.rb
@@ -0,0 +1,7 @@
+module Movie
+  module Premier
+    module Cli
+      VERSION = "0.1.0"
+    end
+  end
+end
