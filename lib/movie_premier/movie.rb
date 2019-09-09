af171e94a57771bf0a470b9d861fbe632eb3d528
MOBOLA MORONTO <t2wycemo@gmail.com>
2019-09-09 17:38:00 +0000

    revised movie.rb

--- lib/movie_premier/movie.rb
+++ lib/movie_premier/movie.rb
@@ -1,48 +1,58 @@
 class MoviePremier::Movie
   attr_accessor :name, :url, :summary, :stars
+  
+  @@all = []
+  
+  #def self.new_from_index_page(r)
+  #self.new(
+      #r.css("h2").text,
+      #"https://www.imdb.com#{r.css("a").attribute("href").text}",
+      #r.css("h3").text,
+      #r.css(".position").text
+      #)
+  #end 
 
-  def initialize(name = nil, url = nil)
+  def initialize(name = nil, url = nil, summary = nil, stars = nil)
     @name = name
     @url = url
+    @summary = summary 
+    @stars = stars
+    @@all << self
   end
 
   def self.all
-    @@all ||= scrape_movie_premier
+    @@all 
   end
 
   def self.find(id)
     self.all[id-1]
   end
 
-  def self.find_by_name(name)
-    self.all.detect do |m|
-      m.name.downcase.strip == name.downcase.strip ||
-      m.name.split("(").first.strip.downcase == name.downcase.strip
-    end
+  def name 
+    @name ||= doc.css("h4 a.").text 
   end
   
+  def url 
+    @url ||= doc.css("https://www.imdb.com/movies-coming-soon/").text 
+  end 
+  
   def summary
-    @summary ||= plot_summary_doc.search("div.outline").text
+    @summary ||= doc.css("div.outline").text
   end
 
   def stars
-    @stars ||= doc.search("#titleCast span[itemprop='name']").collect{|e| e.text.strip}.join(", ")
+    @stars ||= doc.css("#titleCast span[itemprop='name']").collect{|e| e.text.strip}.join(", ")
   end
 
-  private
-    def self.scrape_movie_premier
-      doc = Nokogiri::HTML(open('https://www.imdb.com/movies-coming-soon/'))
-      binding.pry 
-      names = doc.search("h3[itemprop='name'] a[itemprop='url']")
-      names.collect{|e| new(e.text.strip, "http://imdb.com#{e.attr("href").split("?").first.strip}")}
-    end
+  def self.scrape_movie_premier
+    doc = Nokogiri::HTML(open('https://www.imdb.com/movies-coming-soon/'))
+    #binding.pry 
+    names = doc.search("h3[itemprop='name'] a[itemprop='url']")
+    names.collect{|e| new(e.text.strip, "http://imdb.com#{e.attr("href").split("?").first.strip}")}
+  end
 
-    def plot_summary_doc
-      @plot_summary_doc ||= Nokogiri::HTML(open("#{self.url}plotsummary"))
-    end
-
-    def doc
-      @doc ||= Nokogiri::HTML(open(self.url))
-    end
+  def doc
+    @doc ||= Nokogiri::HTML(open(self.url))
+  end
   
 end 
